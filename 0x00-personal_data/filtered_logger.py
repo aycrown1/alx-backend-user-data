0b13d0c27fee22a1160722420e3002ce6347466f
aycrown1 <ayoadeadedamola198@gmail.com>
2024-02-11 00:04:46 +0000

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """This module is a filter logger module
 """
 from typing import List
+import logging
 import re
 
 
@@ -10,6 +11,31 @@ def filter_datum(fields: List[str], redaction: str,
     """ returns the log message obfuscated
     """
     for field in fields:
-        log_message = re.sub(f'{f}=.*?{separator}',
-                             f'{f}={redaction}{separator}', message)
-    return log_message
+        message = re.sub(f'{field}=.*?{separator}',
+                             f'{field}={redaction}{separator}', message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        The constructor method for the class
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        method to filter values in incoming log records using filter_datum.
+            Values for fields in fields should be filtered
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
