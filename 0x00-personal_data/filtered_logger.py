1d77abd9b05dd5c57b97a4484a36a07b43aac2da
aycrown1 <ayoadeadedamola198@gmail.com>
2024-02-11 00:18:26 +0000

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,14 +6,35 @@ import logging
 import re
 
 
-def filter_datum(fields: List[str], redaction: str,
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
                  message: str, separator: str) -> str:
     """ returns the log message obfuscated
     """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
-                             f'{field}={redaction}{separator}', message)
+                         f'{field}={redaction}{separator}', message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    Returns logger object with INFO log level and
+        RedactingFormatter
+            formatter for filtering PII fields
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
